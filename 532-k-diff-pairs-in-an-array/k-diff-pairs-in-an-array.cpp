d14a14385bc551cdcbdc7949e2663e32473f95f4
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-10 18:34:42 +0530

    Time: 11 ms (77.43%) | Memory: 16.1 MB (71.64%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -2,34 +2,6 @@ class Solution
 {
     public:
 
-        bool bsElementFound(vector<int> nums, int start, int target)
-        {
-            int end = nums.size() - 1;
-            if (target > nums[end])
-            {
-                return false;
-            }
-            int mid;
-            while (start <= end)
-            {
-                mid = start + (end - start) / 2;
-
-                if (nums[mid] == target)
-                {
-                    return true;
-                }
-                else if (nums[mid] > target)
-                {
-                    end = mid - 1;
-                }
-                else
-                {
-                    start = mid + 1;
-                }
-            }
-
-            return false;
-        }
 
     int findPairs(vector<int> &nums, int k)
     {
@@ -38,7 +10,7 @@ class Solution
         for (int i = 0; i < nums.size(); i++)
         {
             int elementToFind = nums[i] + k;
-            if (bsElementFound(nums, i + 1, elementToFind)) sets.insert({ nums[i],
+            if (binary_search(nums.begin()+i+1,nums.end(), elementToFind)) sets.insert({ nums[i],
                 elementToFind });
         }
         return sets.size();
