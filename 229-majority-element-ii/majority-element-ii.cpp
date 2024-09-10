a2ca141ec38d5db28787be21463232f6fe266407
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-10 11:37:27 +0530

    Time: 14 ms (16.24%) | Memory: 18.6 MB (77.68%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,59 @@
-class Solution {
-public:
-    vector<int> majorityElement(vector<int>& nums) {
-         unordered_map<int,int> hash;
-        for(auto v: nums){
-            ++hash[v];
-        }
+class Solution
+{
+    public:
+        vector<int> majorityElement(vector<int> &nums)
+        {
 
-        int n = nums.size();
-        vector<int> ans;
-        for(auto pair: hash){
-            if(pair.second>n/3){
-                ans.push_back(pair.first);
+            int majorityCandidate1 = nums[0], vote1 = 0, majorityCandidate2 = -111, vote2 = 0;	// majorityCandidte2  is -1 bcz if all the elemensta are same till end then majorityCandidate2 will never be initialised so instead of using garbage value initialising here to check in candidate validation time bcz sometime garbage  value is zero too and array can contain zero which increase majorityCandidate2 votes without realising that majorityCandidate does not exist 
+
+            for (auto itm: nums)
+            {
+                if (majorityCandidate1 == itm)
+                {
+                    vote1++;
+                }
+                else if (majorityCandidate2 == itm)
+                {
+                    vote2++;
+                }
+                else
+                if (vote1 == 0)
+
+                {
+                    vote1++;
+                    majorityCandidate1 = itm;
+                }
+                else if (vote2 == 0)
+                {
+                    vote2++;
+                    majorityCandidate2 = itm;
+                }
+                else
+                {
+                    vote1--;
+                    vote2--;
+                }
             }
-        }
 
-        return ans;
-    }
+            vote1 = 0;
+            vote2 = 0;
+
+           	// candidate validation
+            for (auto i: nums)
+            {
+                if (i == majorityCandidate1) vote1++;
+            }
+
+            for (auto i: nums)
+            {
+                if (i == majorityCandidate2) vote2++;
+            }
+
+           	//ans
+            vector<int> ans;
+            if (vote1 > nums.size() / 3) ans.push_back(majorityCandidate1);
+            if (vote2 > nums.size() / 3) ans.push_back(majorityCandidate2);
+
+            return ans;
+        }
 };
