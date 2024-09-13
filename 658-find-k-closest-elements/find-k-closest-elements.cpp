d96917e9411bcbaf4cfe3d42bac67fcc5cf12c3f
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-13 20:53:23 +0530

    Time: 27 ms (76.62%) | Memory: 33.9 MB (78.72%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,23 +1,46 @@
 class Solution
 {
     public:
+
         vector<int> findClosestElements(vector<int> &nums, int k, int x)
         {
-            int low = 0;
-            int high = nums.size() - 1;
 
-            while (high - low >= k)
+           	// Find the index of the first element that is not less than x using lower_bound
+            auto highIt = lower_bound(nums.begin(), nums.end(), x) - nums.begin();
+            int n = nums.size();
+
+           	// Set high to the found index, or the last element if x is greater than all elements
+            int high = highIt > n - 1 ? n - 1 : highIt;
+            if (high == 0 && n != 1)
             {
-                if (x - nums[low] > nums[high] - x)
-                {
+                high++;
+            }
 
-                    low++;
+           	// Set low to one element before high
+            int low = high - 1;
+
+           	// Expand the window from the middle until the window contains exactly k elements
+            while (k--)
+            {
+                if (low < 0)
+                {
+                    high++;	// If low is out of bounds, increase high
+                }
+                else if (high >= n)
+                {
+                    low--;	// If high is out of bounds, decrease low
+                }
+                else if (x - nums[low] <= nums[high] - x)
+                {
+                    low--;	// If the left element is closer, move left
                 }
                 else
                 {
-                    high--;
+                    high++;	// Otherwise, move right
                 }
             }
-            return vector<int> (nums.begin() + low, nums.begin() + high + 1);
+
+           	// Return the subarray between low and high (inclusive) containing k closest elements
+            return vector<int> (nums.begin() + low + 1, nums.begin() + high);
         }
 };
