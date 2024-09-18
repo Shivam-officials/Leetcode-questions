cbb1c584b47b500ec7700a4de8294ce705a2080d
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-18 11:46:42 +0530

    Time: 120 ms (25.16%) | Memory: 69.3 MB (40.82%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,54 +1,66 @@
 class Solution
 {
-    public:
-        bool isPossibleSolution(vector<int>&bloomDay,int m ,int k, int mid){
-            int flowerCount  = 0;
-            int bq = 0;
+public:
+    bool isPossibleSolution(vector<int>& bloomDay, int m, int k, int mid)
+    {
+        int flowerCount = 0;
+        int bouquetCount = 0;
 
-            if((long long)m*k>bloomDay.size()){
-                return false;
-            }
-
-            for(int i = 0; i< bloomDay.size();i++){
-                int day = bloomDay[i];
-
-                if(day <= mid){
-                    flowerCount++;
-
-                    if(flowerCount>=k){
-                        bq++;
-                        flowerCount=0;
-                    }
-                }else{
-                    flowerCount = 0;
-                }
-
-                if(bq>=m){
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        int minDays(vector<int> &bloomDay, int m, int k)
+        for (int i = 0; i < bloomDay.size(); i++)
         {
-            int start = *min_element(bloomDay.begin(), bloomDay.end());
-            int end = *max_element(bloomDay.begin(), bloomDay.end());
-            int mid;
-            int ans = -1;
-            while (start <= end)
+            if (bloomDay[i] <= mid)
             {
-                mid = (start + end) >> 1;
-                if (isPossibleSolution(bloomDay, m, k, mid))
+                flowerCount++;
+                if (flowerCount == k)
                 {
-                    end = mid - 1;
-                    ans = mid;
-                }
-                else
-                {
-                    start = mid + 1;
+                    bouquetCount++;
+                    flowerCount = 0;  // Reset flower count for the next bouquet
                 }
             }
-            return ans;
+            else
+            {
+                flowerCount = 0;  // Reset flower count if the flower hasn't bloomed by `mid`
+            }
+
+            if (bouquetCount >= m)
+            {
+                return true;
+            }
         }
+
+        return false;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k)
+    {
+        int n = bloomDay.size();
+
+        // If it's impossible to make `m` bouquets with `k` adjacent flowers, return -1
+        if ((long long)m * k > n)
+        {
+            return -1;
+        }
+
+        int start = *min_element(bloomDay.begin(), bloomDay.end());
+        int end = *max_element(bloomDay.begin(), bloomDay.end());
+        int ans = -1;
+
+        // Binary search for the minimum number of days
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;  // Avoid potential overflow
+
+            if (isPossibleSolution(bloomDay, m, k, mid))
+            {
+                ans = mid;
+                end = mid - 1;  // Try to find a smaller valid day
+            }
+            else
+            {
+                start = mid + 1;  // Increase days to find a valid solution
+            }
+        }
+
+        return ans;
+    }
 };
