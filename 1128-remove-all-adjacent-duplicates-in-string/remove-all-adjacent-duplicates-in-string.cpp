d86ca6fb2a9ecc8c057e493ffb218e09bb672226
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-21 23:34:06 +0530

    Time: 15 ms (83.29%) | Memory: 12.5 MB (63.93%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     string removeDuplicates(string s) {
     
-        int i = 1;
-        while (i<s.length()) {
-            // cout << s<<endl;
-            if(i>= 1 && s.at(i) == s.at(i-1)  )
-            {
-                s.erase(i-1,2);
-                // cout << "erasing the "<<str.at(i)<<"and"<<str.at(i-1)<<endl;
-                // i = i>1?i-1:1;
-                i--;
-            }else 
-            {
-                i++;
-            }
+      
+    string str = "";
+
+    for (auto c : s) {
+        if(str.empty())
+        {
+            str.push_back(c);
+        }else if(c== str.back())
+        {
+            str.pop_back(); 
+        }else 
+        {
+          str.push_back(c);
         }
-        return s;
+        
+    }
+    return str;
+    
+
     }
 };
