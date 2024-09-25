c21cbf51a60c60cc6562c9f93ae3d5412571263c
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-25 23:05:16 +0530

    Time: 102 ms (6.04%) | Memory: 32.6 MB (40.22%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,36 +1,29 @@
-class Solution
-{
-    public:
-        int numberOfBeams(vector<string> &bank)
-        {
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        
+       int totalBeams = 0;
 
-            int totalBeams = 0;
+       int numberOfSecurityDevicesInLastRow = 0;
 
-            int numberOfSecurityDevicesInLastRow = 0;
+       for (int rowIndex = 0; rowIndex< bank.size(); rowIndex++) {
 
-            for (int rowIndex = 0; rowIndex < bank.size(); rowIndex++)
-            {
-
-                string currentRow = bank[rowIndex];
-
-                int currentSecurityDeviceNumbers = 0;
-                for (auto cell: currentRow)
-                {
-                    if (cell == '1')
-                    {
-                        currentSecurityDeviceNumbers++;
-                    }
-                }
-
-                if (currentSecurityDeviceNumbers > 0)
-                {
-                    if (rowIndex > 0)
-                    {
-                        totalBeams += currentSecurityDeviceNumbers * numberOfSecurityDevicesInLastRow;
-                    }
-                    numberOfSecurityDevicesInLastRow = currentSecurityDeviceNumbers;
-                }
+        string currentRow = bank[rowIndex];
+        
+        int currentSecurityDeviceNumbers = 0;
+        for (auto cell : currentRow) {
+            if (cell == '1') {
+                currentSecurityDeviceNumbers++;
             }
-            return totalBeams;
         }
+
+        if (currentSecurityDeviceNumbers>0 ) {
+           if (rowIndex>0) {
+           totalBeams += currentSecurityDeviceNumbers*numberOfSecurityDevicesInLastRow;
+           }
+           numberOfSecurityDevicesInLastRow = currentSecurityDeviceNumbers;
+        }
+       }
+ return totalBeams;
+    }
 };
