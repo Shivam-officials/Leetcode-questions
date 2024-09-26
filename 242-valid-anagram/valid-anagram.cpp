c944fb87bec8fa8ce7ce857f41977ef51fe7c7bd
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-26 12:42:34 +0530

    Time: 4 ms (89.72%) | Memory: 8.9 MB (55.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,34 +1,29 @@
-class Solution {
-public:
+class Solution
+{
+    public:
 
-    
-    bool isAnagram(string s , string t) {
-    
+        bool isAnagram(string firstStr, string SecondStr)
+        {
+            vector<int> freq(256, 0);	// Frequency array for ASCII characters
 
-int freqTable[256] = {0};
+            for (auto ch: firstStr)
+            {
+                freq[ch]++;	// Count frequency of each char in first string
+            }
 
+            for (auto ch: SecondStr)
+            {
+                if (freq[ch] == 0)
+                    return false;	// Extra char in second string
+                freq[ch]--;	// Decrement freq for matching chars
+            }
 
-for (auto ch : s) {
-    freqTable[ch]++;
-}
+            for (auto elm: freq)
+            {
+                if (elm != 0)
+                    return false;	// Remaining unmatched chars
+            }
 
-for (auto ch : t) {
-    // if(freqTable[ch]==0)
-    // {
-    //     return false;
-    // }
-    
-    freqTable[ch]--;
-}
-
-for (auto elm : freqTable) {
-    if (elm !=0) {
-     return false;
-    }
-}
-
- return true;   
-
-
-    }
+            return true;	// Strings are anagrams
+        }
 };
