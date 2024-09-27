60b5532bb2946816f26f6f9ebd55ae7938102da1
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-27 11:43:31 +0530

    Time: 6 ms (64.09%) | Memory: 8.5 MB (72.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
 
-    void normalise(string &s){
-        unordered_map<char,char> mapping;
-        char start = 'a';
-        // create the mapping
-        for(auto ch:s){
-            if(mapping.find(ch)==mapping.end()){
-                mapping[ch] = start++;
-            }
-        }
-
-        // update the string
-        for(auto &ch: s){
-            ch = mapping[ch];
-        }
-
-    }
-
     bool isIsomorphic(string s, string t) {
-        if(s.length()!= t.length()) return false;
-        normalise(s);
-        normalise(t);
-        if(s.compare(t)==0){
-            return true;
-        }
 
-        return false;
+  int n = s.size();
+  int m1[256] = {0}, m2[256] = {0};
+
+  for (int i = 0; i < n; i++) {
+    if (m1[s[i]] != m2[t[i]])
+      return false;
+
+    else {
+      m1[s[i]] = i + 1;
+      m2[t[i]] = i + 1;
     }
+  }
+
+  return true;
+}
 };
