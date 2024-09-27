1ffb6d460dd638a12e137aedce6ebaddaa5bd0db
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-27 10:43:14 +0530

    Time: 10 ms (31.08%) | Memory: 9.4 MB (78.79%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -3,10 +3,11 @@ class Solution
     public:
 
         bool isThisAVowel(char c)
-        {
+        {    
+            c = tolower(c);
            	// Checks if the character is an vowel (both uppercase and lowercase)
             
-            return (c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u' ||c == 'A' ||c == 'E' ||c == 'I' ||c == 'O' ||c == 'U');
+            return (c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u' );
         }
     string reverseVowels(string s)
     {
