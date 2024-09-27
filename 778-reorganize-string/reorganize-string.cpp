9e00093db50a755385caa30b9f1b8ab74d3fade3
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-27 22:43:38 +0530

    Time: 4 ms (33.24%) | Memory: 8.9 MB (33.47%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,46 +1,49 @@
-class Solution {
-public:
-    string reorganizeString(string s) {
-        unordered_map<char, int> count;
-    // find the most occusring element
-    int maxOccuringElementCount = INT_MIN;
-    char maxOccuringElement ;
-    for (auto ch : s) {
-        count[ch]++;
-       if(maxOccuringElementCount<count[ch])
-       {
-        maxOccuringElementCount = count[ch];
-        maxOccuringElement = ch;
-       }
-       
-    }
-
-    // place the most occuring element
-    int index = 0;
-    while(count[maxOccuringElement]--)
-    {
-        if(index >= s.length())
+class Solution
+{
+    public:
+        string reorganizeString(string s)
         {
-            return "";
-        }
-        
-        s[index] = maxOccuringElement;
-        index += 2;
-    }
+            unordered_map<char, int> count;
+           	// find the most occusring element
+            int maxOccuringElementCount = INT_MIN;
+            char maxOccuringElement;
+            for (auto ch: s)
+            {
+                count[ch]++;
+                if (maxOccuringElementCount < count[ch])
+                {
+                    maxOccuringElementCount = count[ch];
+                    maxOccuringElement = ch;
+                }
+            }
 
-    count[maxOccuringElement]=0;
-    // place the rest
-    for (auto &occurance : count) {
-        while (occurance.second--) {
-            // cout << occurance.second<<endl;
-            index = index>=s.length()? 1: index;
+           	// place the most occuring element
+            int index = 0;
+            while (count[maxOccuringElement]--)
+            {
+                if (index >= s.length())
+                {
+                    return "";
+                }
 
-            s[index] = occurance.first;
-            index += 2;
+                s[index] = maxOccuringElement;
+                index += 2;
+            }
+
+            count[maxOccuringElement] = 0;
+           	// place the rest
+            for (auto &occurance: count)
+            {
+                while (occurance.second--)
+                {
+                   	// cout << occurance.second<<endl;
+                    index = index >= s.length() ? 1 : index;
+
+                    s[index] = occurance.first;
+                    index += 2;
+                }
+            }
+           	// cout << s;
+            return s;
         }
-       
-    }
-    // cout << s;
-    return s;
-    }
 };
