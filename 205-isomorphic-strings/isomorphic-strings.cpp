6d0fc03f4a480225533fd847a3ba503488c404c4
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-27 11:34:32 +0530

    Time: 3 ms (90.47%) | Memory: 8.6 MB (66.44%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -19,13 +19,22 @@ public:
     }
 
     bool isIsomorphic(string s, string t) {
-        // if(s.length()!= t.length()) return false;
-        normalise(s);
-        normalise(t);
-        if(s.compare(t)==0){
-            return true;
-        }
+        // normallsing method
 
-        return false;
+  int n = s.size();
+  int m1[256] = {0}, m2[256] = {0};
+
+  for (int i = 0; i < n; i++) {
+    if (m1[s[i]] != m2[t[i]])
+      return false;
+
+    else {
+      m1[s[i]] = i + 1;
+      m2[t[i]] = i + 1;
+    }
+  }
+
+  return true;
+
     }
 };
