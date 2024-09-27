6a0536a96e7fac24b163bec2a103e3f91c20b80d
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-27 22:43:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (54.44%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> count;
+    // find the most occusring element
+    int maxOccuringElementCount = INT_MIN;
+    char maxOccuringElement ;
+    for (auto ch : s) {
+        count[ch]++;
+       if(maxOccuringElementCount<count[ch])
+       {
+        maxOccuringElementCount = count[ch];
+        maxOccuringElement = ch;
+       }
+       
+    }
+
+    // place the most occuring element
+    int index = 0;
+    while(count[maxOccuringElement]--)
+    {
+        if(index >= s.length())
+        {
+            return "";
+        }
+        
+        s[index] = maxOccuringElement;
+        index += 2;
+    }
+
+    count[maxOccuringElement]=0;
+    // place the rest
+    for (auto &occurance : count) {
+        while (occurance.second--) {
+            // cout << occurance.second<<endl;
+            index = index>=s.length()? 1: index;
+
+            s[index] = occurance.first;
+            index += 2;
+        }
+       
+    }
+    // cout << s;
+    return s;
+    }
+};
