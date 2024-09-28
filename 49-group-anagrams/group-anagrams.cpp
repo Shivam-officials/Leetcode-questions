9172c5dcce20a2d0bb12f153a2ac184ed251d753
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-28 23:07:16 +0530

    Time: 68 ms (11.82%) | Memory: 46.8 MB (5.13%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,25 +1,33 @@
 class Solution
 {
     public:
-        vector<vector < string>> groupAnagrams(vector<string> &strs)
+        array<int, 256> makeFrequencyTable(string s)
         {
-            unordered_map<string, vector < string>> anagramGroupsTable;
-
-           	// making the table
-            for (auto ch: strs)
+            array<int, 256> frequencyTable = {0};
+            for (auto ch: s)
             {
-                string s = ch;
-                sort(s.begin(), s.end());
-                anagramGroupsTable[s].push_back(ch);
+                frequencyTable[ch]++;
             }
-
-           	// making the groups
-            vector<vector < string>> ans;
-            for (auto it: anagramGroupsTable)
-            {
-                ans.push_back(it.second);
-            }
-
-            return ans;
+            return frequencyTable;
         }
+    vector<vector < string>> groupAnagrams(vector<string> &strs)
+    {
+    	// HashMap to store sorted strings as keys and original strings as values (grouped anagrams)
+        map<array<int, 256>, vector< string >> anagramGroupsTable;
+
+       	// For each string, sort it and use the sorted string as the key
+        for (auto currentWord: strs)
+        {
+            anagramGroupsTable[makeFrequencyTable(currentWord)].push_back(currentWord);	// Insert original string -> O(1) average
+        }
+
+       	// Build the result vector of grouped anagrams
+        vector<vector < string>> ans;
+        for (auto it: anagramGroupsTable)
+        {
+            ans.push_back(it.second);	// Add each group to the result -> O(n)
+        }
+
+        return ans;
+    }
 };
