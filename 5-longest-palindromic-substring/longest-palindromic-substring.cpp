284f264deeb9eb7feb46ddaa60d4133a947f0c89
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-09-29 19:48:38 +0530

    Time: 67 ms (48.57%) | Memory: 174.1 MB (20.94%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string palindromeAroundIt(string s, int low, int high){
+        while(low>=0 && high < s.length() && s[low]== s[high]){
+            low--;
+            high++;
+        }
+        return s.substr(low+1,high-low-1);
+    }
+    string longestPalindrome(string s) {
+        string longest = "";
+        for(int i = 0; i< s.length();i++){
+            string oddLengthLongestPalindrome = palindromeAroundIt(s,i,i);
+            string evenLengthLongestPalindrome = palindromeAroundIt(s,i,i+1);
+
+            if(longest.length()<oddLengthLongestPalindrome.length()) {
+                longest = oddLengthLongestPalindrome;
+            }
+
+            if(longest.length() < evenLengthLongestPalindrome.length()){
+                longest = evenLengthLongestPalindrome;
+            }
+        }
+
+        return longest;
+    }
+};
