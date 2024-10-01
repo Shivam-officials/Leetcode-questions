9a5e8f389be89ebe8f3d1f988fbd25e5f00e5d6d
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-10-01 11:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (27.90%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -3,52 +3,33 @@ class Solution
     public:
 
         int myAtoi(string s)
-        {
-            int number = 0;
+        { 
+            int i = 0;
+           // ignoring white space
+           while(s[i]==' '){
+            i++;
+           }
+
+            // sign cheking
             int sign = 1;
-            bool numStarted = false;	// Flag to check when to start processing digits
+           if(s[i]=='-' || s[i]=='+'){
+            sign = s[i]=='-' ? -1 : 1;
+            i++;
+           }
 
-            for (auto ch: s)
-            {
-               	// Skip leading spaces
-                if (ch == ' ' && !numStarted)
-                {
-                    continue;
-                }
-
-               	// Handle sign if it's the first non-space character
-                if (ch == '-' && !numStarted)
-                {
-                    sign = -1;
-                    numStarted = true;	// Mark that we've started processing
-                    continue;
-                }
-                else if (ch == '+' && !numStarted)
-                {
-                    numStarted = true;
-                    continue;
-                }
-
-               	// Stop if non-digit characters are found (including alphabets and '.')
-                if (ch<'0' || ch > '9')
-                {
-                    break;
-                }
-
-               	// Convert char to digit
-                int digit = ch - '0';
-
-               	// Overflow check before processing the next digit
-                if (number > (INT_MAX - digit) / 10)
-                {
-                    return sign == 1 ? INT_MAX : INT_MIN;
-                }
-
-               	// Accumulate the digit into the number
-                number = number *10 + digit;
-                numStarted = true;	// Mark that we've started processing
+           // making the number
+           int num = 0;
+           while(i<s.length() && isdigit(s[i])){
+            int digit = s[i]-'0';
+            // cheking overflow
+            if(num > (INT_MAX - digit)/10){
+                return sign == -1 ? INT_MIN : INT_MAX;
             }
+            num = (num * 10) + digit;
+            i++;
+           }
+           
+            return sign * num;
 
-            return sign * number;
         }
 };
