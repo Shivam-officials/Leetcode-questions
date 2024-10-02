c33ffde735d32a152bf8753a9137a866a21eaf69
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-10-02 22:12:51 +0530

    Time: 19 ms (32.10%) | Memory: 14 MB (35.34%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+        string convert(string s, int numRows)
+        {
+            vector<string> zigzag
+            {
+                (size_t) numRows, "" };
+
+            string ans = "";
+
+            if (numRows == 1) return s;
+            
+            int i = 0;
+            bool direction = 1;
+            int row = 0;
+            while (true)
+            {
+                if (direction)
+                {
+                    while (i < s.size() && row < numRows)
+                    {
+                        zigzag[row++].push_back(s[i++]);
+                       	// cout << "the i is the " << i << endl;
+                    }
+                    direction = !direction;
+                    row -= 2;
+                }
+                else
+                {
+                    while (i < s.size() && row >= 0)
+                    {
+                        zigzag[row--].push_back(s[i++]);
+                    }
+                    row = 1;
+                    direction = !direction;
+                }
+
+                if (i >= s.size())
+                {
+                    break;
+                }
+            }
+
+           	// cout << zigzag[0] << zigzag[1] << zigzag[2] << zigzag[3];
+            for (auto str: zigzag)
+            {
+                ans += str;
+            }
+            return ans;
+        }
+};
