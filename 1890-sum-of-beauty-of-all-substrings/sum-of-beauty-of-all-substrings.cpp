5baea44f93ae5808537f9b0fd40e4fe0ee8b4e5e
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-10-04 23:14:43 +0530

    Time: 50 ms (97.75%) | Memory: 7.9 MB (95.87%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -3,9 +3,9 @@ public:
     int beautySum(string s) {
       int beutySum = 0;
   for (int i = 0; i < s.length(); i++) {
-    int hash[256] = {0};
+    int hash[26] = {0};
     for (int j = i; j < s.length(); j++) {
-      hash[s[j]]++;
+      hash[s[j] - 'a']++;
 
       int mini = INT_MAX;
       int maxi = INT_MIN;
