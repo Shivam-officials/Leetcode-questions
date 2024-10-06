100930c7d6667902dbcc7ed3de6c92f567b9a32b
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-10-06 17:36:40 +0530

    Time: 3 ms (74.15%) | Memory: 13.7 MB (25.50%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // Track the current number and initialize pointer p for tracking the next position
-    int currentNum = nums[0]; // Start with the first number
-    int p = 0; // Position tracking where the next valid element will be placed
-    int count = 0; // Count occurrences of the current number
-
-    // Iterate through the array
-    for (int i = 0; i < nums.size();) {
-        // Visit each integer and count its occurrences
-        while (i < nums.size() && currentNum == nums[i]) {
-            count++; // Count this occurrence
-            // If occurrence is less than or equal to 2, place it at position p
-            if (count <= 2) {
-                nums[p++] = nums[i]; // Place the valid number at position p and increment p
+        int num = nums[0];
+        int p = 0;
+            
+        int count = 0;
+        for(int i = 0; i< nums.size();){
+            while(i<nums.size() && num == nums[i]){
+                count++;
+                if(count<=2){
+                    nums[p++]=nums[i];
+                }
+                i++;
             }
-            i++; // Move to the next number
+            if(i==nums.size())break;
+            nums[p++] = nums[i];
+            num = nums[i++];
+            count = 1;
+
         }
-
-        // If we've reached the end of the array, exit the loop
-        if (i == nums.size()) break;
-
-        // Store the next different number at position p
-        nums[p++] = nums[i]; // Place the next unique number at position p
-        currentNum = nums[i++]; // Update current number for counting its occurrences
-        count = 1; // Reset count for the new number
-    }
-    
-    return p; // Return the count of valid elements (at most two duplicates)
+        return p;
     }
 };
