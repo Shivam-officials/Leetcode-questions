c95a2f4a90db307f276e63166737ba211a494a00
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-10-09 22:00:41 +0530

    Time: 5 ms (46.05%) | Memory: 15.3 MB (80.15%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -9,13 +9,14 @@ public:
     
         if(a == 0)return b;
         if(b==0)return a;
-     while(a>0 && b >0){
-        if(a>b){
-            a = a-b;
-        }else {
-            b = b-a;
+    while(a>0 && b >0){
+            if(a>b) {
+                a = a%b;
+            }else{
+                b = b%a;
+            }
         }
-     }
+     
      return a==0 ? b:a;
     }
 };
