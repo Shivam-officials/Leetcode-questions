8896fdc1bdfd535fcf7237c516f0e77e0f336ec2
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-11-04 23:36:40 +0530

    Time: 6 ms (23.96%) | Memory: 10.2 MB (85.43%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -1,129 +1,82 @@
 class Solution
 {
     public:
-        string numberToWords_bySir(int num, vector<pair<int, string>> &mp)
+        string numberToWords(int num)
         {
-            
-
-                if (num == 0) return "Zero";
-
-                for (auto it: mp)
-                {
-                    if (num >= it.first)
-                    {
-
-                        string a = "";
-                        if (num >= 100) a = numberToWords_bySir(num / it.first, mp) + " ";
-
-                        string b = it.second;
-
-                        string c = "";
-                        if (num % it.first != 0) c = " " + numberToWords_bySir(num % it.first, mp);
-
-                        return a + b + c;
-                    }
-                }
-                return "";
-            }
-
-            string numberToWords(int num)
+            if (num == 0) return "Zero";
+            string ans;
+            int batchIndx = 0;
+            while (num > 0)
             {
-                vector<pair<int, string>> mp
+                if (num % 1000 != 0)
                 {
-                    {
-                        1000000000,
-                        "Billion" },
-                    {
-                        1000000,
-                        "Million" },
-                    {
-                        1000,
-                        "Thousand" },
-                    {
-                        100,
-                        "Hundred" },
-                    {
-                        90,
-                        "Ninety" },
-                    {
-                        80,
-                        "Eighty" },
-                    {
-                        70,
-                        "Seventy" },
-                    {
-                        60,
-                        "Sixty" },
-                    {
-                        50,
-                        "Fifty" },
-                    {
-                        40,
-                        "Forty" },
-                    {
-                        30,
-                        "Thirty" },
-                    {
-                        20,
-                        "Twenty" },
-                    {
-                        19,
-                        "Nineteen" },
-                    {
-                        18,
-                        "Eighteen" },
-                    {
-                        17,
-                        "Seventeen" },
-                    {
-                        16,
-                        "Sixteen" },
-                    {
-                        15,
-                        "Fifteen" },
-                    {
-                        14,
-                        "Fourteen" },
-                    {
-                        13,
-                        "Thirteen" },
-                    {
-                        12,
-                        "Twelve" },
-                    {
-                        11,
-                        "Eleven" },
-                    {
-                        10,
-                        "Ten" },
-                    {
-                        9,
-                        "Nine" },
-                    {
-                        8,
-                        "Eight" },
-                    {
-                        7,
-                        "Seven" },
-                    {
-                        6,
-                        "Six" },
-                    {
-                        5,
-                        "Five" },
-                    {
-                        4,
-                        "Four" },
-                    {
-                        3,
-                        "Three" },
-                    {
-                        2,
-                        "Two" },
-                    {
-                        1,
-                        "One" },
-                };
-                return numberToWords_bySir(num, mp);
+                    string batchAns = processBatch(num % 1000, batchIndx);
+                    ans = batchAns + ans;
+                }
+                num /= 1000;
+                batchIndx++;
             }
-        };
+            ans.erase(ans.find_last_not_of(" ") + 1);
+            return ans;
+        }
+
+    private:
+        string processBatch(int currBatch, int batchIndx)
+        {
+            const string thousandsAndBeyond[] = { "",
+                "Thousand",
+                "Million",
+                "Billion" };
+            const string tens[] = { "",
+                "",
+                "Twenty",
+                "Thirty",
+                "Forty",
+                "Fifty",
+                "Sixty",
+                "Seventy",
+                "Eighty",
+                "Ninety" };
+            const string ones[] = { "",
+                "One",
+                "Two",
+                "Three",
+                "Four",
+                "Five",
+                "Six",
+                "Seven",
+                "Eight",
+                "Nine",
+                "Ten",
+                "Eleven",
+                "Twelve",
+                "Thirteen",
+                "Fourteen",
+                "Fifteen",
+                "Sixteen",
+                "Seventeen",
+                "Eighteen",
+                "Nineteen" };
+
+            string batchAns;
+            if (currBatch >= 100)
+            {
+                int hunsIndx = currBatch / 100;
+                batchAns += ones[hunsIndx] + " Hundred ";
+                currBatch %= 100;
+            }
+            if (currBatch >= 20)
+            {
+                int tensIndx = currBatch / 10;
+                batchAns += tens[tensIndx] + " ";
+                currBatch %= 10;
+            }
+            if (currBatch > 0)
+            {
+                int onesIndx = currBatch;
+                batchAns += ones[onesIndx] + " ";
+            }
+            batchAns += thousandsAndBeyond[batchIndx] + " ";
+            return batchAns;
+        }
+};
