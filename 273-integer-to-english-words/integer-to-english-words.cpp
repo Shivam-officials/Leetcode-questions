a13ceb0d675408bc540de3d6ea967b0762109913
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-11-04 18:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (37.16%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -1,105 +1,8 @@
 class Solution
 {
     public:
-        string numberToWords_bySir(int num)
+        string numberToWords_bySir(int num, vector<pair<int, string>> &mp)
         {
-
-            vector<pair<int, string>> mp
-            {
-                {
-                    1000000000,
-                    "Billion" },
-                {
-                    1000000,
-                    "Million" },
-                {
-                    1000,
-                    "Thousand" },
-                {
-                    100,
-                    "Hundred" },
-                {
-                    90,
-                    "Ninety" },
-                {
-                    80,
-                    "Eighty" },
-                {
-                    70,
-                    "Seventy" },
-                {
-                    60,
-                    "Sixty" },
-                {
-                    50,
-                    "Fifty" },
-                {
-                    40,
-                    "Forty" },
-                {
-                    30,
-                    "Thirty" },
-                {
-                    20,
-                    "Twenty" },
-                {
-                    19,
-                    "Nineteen" },
-                {
-                    18,
-                    "Eighteen" },
-                {
-                    17,
-                    "Seventeen" },
-                {
-                    16,
-                    "Sixteen" },
-                {
-                    15,
-                    "Fifteen" },
-                {
-                    14,
-                    "Fourteen" },
-                {
-                    13,
-                    "Thirteen" },
-                {
-                    12,
-                    "Twelve" },
-                {
-                    11,
-                    "Eleven" },
-                {
-                    10,
-                    "Ten" },
-                {
-                    9,
-                    "Nine" },
-                {
-                    8,
-                    "Eight" },
-                {
-                    7,
-                    "Seven" },
-                {
-                    6,
-                    "Six" },
-                {
-                    5,
-                    "Five" },
-                {
-                    4,
-                    "Four" },
-                {
-                    3,
-                    "Three" },
-                {
-                    2,
-                    "Two" },
-                {
-                    1,
-                    "One" },
-            };
 
             if (num == 0) return "Zero";
 
@@ -109,12 +12,12 @@ class Solution
                 {
 
                     string a = "";
-                    if (num >= 100) a = numberToWords_bySir(num / it.first) + " ";
+                    if (num >= 100) a = numberToWords_bySir(num / it.first, mp) + " ";
 
                     string b = it.second;
 
                     string c = "";
-                    if (num % it.first != 0) c = " " + numberToWords_bySir(num % it.first);
+                    if (num % it.first != 0) c = " " + numberToWords_bySir(num % it.first, mp);
 
                     return a + b + c;
                 }
@@ -122,7 +25,104 @@ class Solution
             return "";
         }
 
-    string numberToWords(int num) {
-        return numberToWords_bySir(num);
+    string numberToWords(int num)
+    {
+        vector<pair<int, string>> mp
+        {
+            {
+                1000000000,
+                "Billion" },
+            {
+                1000000,
+                "Million" },
+            {
+                1000,
+                "Thousand" },
+            {
+                100,
+                "Hundred" },
+            {
+                90,
+                "Ninety" },
+            {
+                80,
+                "Eighty" },
+            {
+                70,
+                "Seventy" },
+            {
+                60,
+                "Sixty" },
+            {
+                50,
+                "Fifty" },
+            {
+                40,
+                "Forty" },
+            {
+                30,
+                "Thirty" },
+            {
+                20,
+                "Twenty" },
+            {
+                19,
+                "Nineteen" },
+            {
+                18,
+                "Eighteen" },
+            {
+                17,
+                "Seventeen" },
+            {
+                16,
+                "Sixteen" },
+            {
+                15,
+                "Fifteen" },
+            {
+                14,
+                "Fourteen" },
+            {
+                13,
+                "Thirteen" },
+            {
+                12,
+                "Twelve" },
+            {
+                11,
+                "Eleven" },
+            {
+                10,
+                "Ten" },
+            {
+                9,
+                "Nine" },
+            {
+                8,
+                "Eight" },
+            {
+                7,
+                "Seven" },
+            {
+                6,
+                "Six" },
+            {
+                5,
+                "Five" },
+            {
+                4,
+                "Four" },
+            {
+                3,
+                "Three" },
+            {
+                2,
+                "Two" },
+            {
+                1,
+                "One" },
+        };
+        return numberToWords_bySir(num, mp);
     }
 };
