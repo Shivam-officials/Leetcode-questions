9eae3a3109bf204bc2f1e2fe6e1dbaea2e0dc3da
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-11-19 22:54:40 +0530

    Time: 3 ms (74.56%) | Memory: 13.6 MB (68.60%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
+    int solveIt(vector<int> &v,int maximumTime) {
+  int next = 1, times = 1;
+  int matcher = v[0];
+
+  int k = 1; 
+  // chalte jao kuch bhi 2 times se kam mile piche feko aur kuch nya mile to bhi piche fek do aur times ko nya hai to 1 kr do
+  while (k < v.size()) {
+    if (v[k] == matcher) {
+        times++;
+      if (times <= maximumTime) {
+        v[next++] = v[k];
+      }
+    } else {
+      matcher = v[k];
+      v[next++] = v[k];
+      times = 1;
+    }
+    k++;
+  }
+  // remove rest of them
+
+  return next;
+}
     int removeDuplicates(vector<int>& nums) {
-     unordered_map<int,int>mp;
-     int index = 0;
-     for(auto ch:nums){
-        if(mp.find(ch)==mp.end()){
-            nums[index++] = ch;
-            mp[ch]++;
-        }else{
-            if(mp[ch]==1){
-                mp[ch]++;
-                nums[index++]=ch;
-            }
-        }
-     }
-     return index;
+    //  unordered_map<int,int>mp;
+    //  int index = 0;
+    //  for(auto ch:nums){
+    //     if(mp.find(ch)==mp.end()){
+    //         nums[index++] = ch;
+    //         mp[ch]++;
+    //     }else{
+    //         if(mp[ch]==1){
+    //             mp[ch]++;
+    //             nums[index++]=ch;
+    //         }
+    //     }
+    //  }
+    //  return index;
+
+    return solveIt(nums,2);
     }
 };
