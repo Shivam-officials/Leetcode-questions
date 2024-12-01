3b6a5d9b683fcae8430fe5e24e252b81b7dd958b
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-12-01 13:35:14 +0530

    Time: 3 ms (69.83%) | Memory: 13 MB (75.65%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     
-void solve_shivam(vector<string> & ans, int n, int openingParenthesis,int closingParenthesis,string outputString){
+void solve_shivam(vector<string> & ans, int n, int openingParenthesis,int closingParenthesis,string &outputString){
     if(closingParenthesis > openingParenthesis) return;
     if(closingParenthesis > n || openingParenthesis > n) return;
     // cout << "it runned"<<endl;
@@ -14,15 +14,19 @@ void solve_shivam(vector<string> & ans, int n, int openingParenthesis,int closin
 
 
     // cout << "it runned3"<<endl;
-    solve_shivam(ans,n, openingParenthesis+1, closingParenthesis, outputString+"(");
-    // cout << outputString+"("<<" ";
-    solve_shivam(ans,n, openingParenthesis, closingParenthesis + 1, outputString+")");
-    // cout << outputString+")"<<" ";
+    outputString.push_back('(');
+    solve_shivam(ans,n, openingParenthesis+1, closingParenthesis, outputString);
+    outputString.pop_back();
+
+    outputString.push_back(')');
+    solve_shivam(ans,n, openingParenthesis, closingParenthesis + 1, outputString);
+    outputString.pop_back();
     return;
 }
     vector<string> generateParenthesis(int n) {
      vector<string>ans ;
-    solve_shivam(ans,n,1,0,"(");
+     string outputString = "(";
+    solve_shivam(ans,n,1,0,outputString);
 
     return ans;
  
