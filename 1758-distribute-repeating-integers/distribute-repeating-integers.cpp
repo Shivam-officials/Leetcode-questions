5f1d7294376844085ed6ca12300caefef87d5a22
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2024-12-15 01:49:30 +0530

    Time: 1142 ms (6.27%) | Memory: 394.2 MB (5.43%) - LeetSync

--- 1758-distribute-repeating-integers/distribute-repeating-integers.cpp
+++ 1758-distribute-repeating-integers/distribute-repeating-integers.cpp
@@ -1,46 +1,51 @@
-class Solution {
-public:
+class Solution
+{
+    public:
 
-bool canDistributeHelper_Sir(vector<int>&nums, vector<int>&quantity, vector<int> occurances, int start){
-    //base case 
-    if(start == quantity.size()){
-        return true;
-    }
+        bool canDistributeHelper_Sir(vector<int> &quantity, vector< int > occurances, int start)
+        {
+           	//base case 
+            if (start == quantity.size())
+            {
+                return true;
+            }
 
-    int currentQuantity = quantity[start];
+            int currentQuantity = quantity[start];
 
-    for (auto &occur : occurances) {
-        if (currentQuantity<=occur) {
-         occur = occur-currentQuantity;
-        //  visited[start] = true;
-         if(canDistributeHelper_Sir(nums, quantity,  occurances, start+1)){
-            return true;
-         }
-        // visited[start] = false;
-        occur = occur+currentQuantity;
+            for (auto &occur: occurances)
+            {
+                if (currentQuantity <= occur)
+                {
+                    occur = occur - currentQuantity;
+                   	//  visited[start] = true;
+                    if (canDistributeHelper_Sir( quantity, occurances, start + 1))
+                    {
+                        return true;
+                    }
+                   	// visited[start] = false;
+                    occur = occur + currentQuantity;
+                }
+            }
+            return false;
         }
+
+    bool canDistribute(vector<int> &nums, vector<int> &quantity)
+    {
+        bool ans = false;
+        unordered_map<int, int> occurances;
+        vector<int> count;
+        for (auto &elm: nums)
+        {
+            occurances[elm]++;
+        }
+        for (auto &pair: occurances)
+        {
+            count.push_back(pair.second);
+           	//    cout << pair.first<<" "<<pair.second <<endl;
+        }
+
+        sort(quantity.rbegin(), quantity.rend());
+
+        return canDistributeHelper_Sir(quantity, count, 0);
     }
-    return false;
-}
-
-
-bool canDistribute(vector<int>& nums, vector<int>& quantity) {
-  bool ans = false;
-  unordered_map<int, int>occurances;
-  vector<int>count;
-  for (auto &elm : nums) {
-   occurances[elm]++;
-  }
-  for (auto &pair : occurances) {
-    count.push_back(pair.second);
-//    cout << pair.first<<" "<<pair.second <<endl;
-  }
-  
-
-
-   sort(quantity.rbegin(),quantity.rend());
-  
-
-  return canDistributeHelper_Sir(nums,quantity,count,0);      
-}
 };
