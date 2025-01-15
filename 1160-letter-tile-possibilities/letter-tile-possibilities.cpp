c6896bf5990769b7689c3f2ce17fdee554344d85
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2025-01-15 11:12:58 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (76.90%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,45 +1,65 @@
 class Solution
 {
     public:
+        /**method 1
+                void subSequence(string &tiles, set<string> &ansSet, string outputStr, int index)
+                {
+                   	//base case
+                    if (index == tiles.length())
+                    {
+                        ansSet.insert(outputStr);
+                        return;
+                    }
 
-        void subSequence(string &tiles, set<string> &ansSet, string outputStr, int index)
-        {
-           	//base case
-            if (index == tiles.length())
+                   	//include
+                    subSequence(tiles, ansSet, outputStr + tiles[index], index + 1);
+                    subSequence(tiles, ansSet, outputStr, index + 1);
+                }
+
+            void permutations(string &tiles, set<string> &ans, int index)
             {
-                ansSet.insert(outputStr);
-                return;
+               	// base case
+                if (index == tiles.length())
+                {
+                    ans.insert(tiles);
+                    return;
+                }
+
+                unordered_map<char, bool> map;
+               	// recursion process
+                for (int i = index; i < tiles.length(); i++)
+                {
+
+                    if (map.find(tiles[i]) != map.end()) continue;
+                    map[tiles[i]] = true;
+                    swap(tiles[i], tiles[index]);
+                    permutations(tiles, ans, index + 1);
+                    swap(tiles[i], tiles[index]);
+                }
             }
 
-           	//include
-            subSequence(tiles, ansSet, outputStr + tiles[index], index + 1);
-            subSequence(tiles, ansSet, outputStr, index + 1);
-        }
+        */
 
-    void permutations(string &tiles, set<string> &ans, int index)
-    {
-       	// base case
-        if (index == tiles.length())
+        void allPossibleArrangement(unordered_map<char, int> &count, int &result)
         {
-            ans.insert(tiles);
+           	//base case
+           	// cout << result<<endl;
+            for (auto &pair: count)
+            {
+                if (pair.second > 0)
+                {
+                    result++;
+                    pair.second--;
+                    allPossibleArrangement(count, result);
+                    pair.second++;
+                }
+            }
             return;
         }
 
-        unordered_map<char, bool> map;
-       	// recursion process
-        for (int i = index; i < tiles.length(); i++)
-        {
-
-            if (map.find(tiles[i]) != map.end()) continue;
-            map[tiles[i]] = true;
-            swap(tiles[i], tiles[index]);
-            permutations(tiles, ans, index + 1);
-            swap(tiles[i], tiles[index]);
-        }
-    }
-
     int numTilePossibilities(string tiles)
     {
+        /**method 1
         set<string> ans;
         subSequence(tiles, ans, "", 0);
         set<string> realAns;
@@ -47,8 +67,18 @@ class Solution
         {
             permutations(elm, realAns, 0);
            	// cout<<elm<<endl;
-           
         }
          return realAns.size() - 1;
+         */
+
+        unordered_map<char, int> count;
+        for (auto elm: tiles)
+        {
+            count[elm]++;
+        }
+
+        int result = 0;
+        allPossibleArrangement(count, result);
+        return result;
     }
 };
