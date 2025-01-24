e40208ae3147db1ff0e2d8f5ad221d8a2ccca051
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2025-01-24 10:52:17 +0530

    Time: 13 ms (18.24%) | Memory: 11.8 MB (76.79%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,16 +9,37 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,bool> map;
-        ListNode* cur = head;
-        while(cur != nullptr){
-            if(map.find(cur) != map.end() ){
-                return true;
+        // unordered_map<ListNode*,bool> map;
+        // ListNode* cur = head;
+        // while(cur != nullptr){
+        //     if(map.find(cur) != map.end() ){
+        //         return true;
+        //     }
+
+        //     map[cur] = true;
+        //     cur = cur->next;
+
+        // }
+        // return false;
+
+        //edge cases
+        if(head == nullptr)return false;
+        if(head->next==nullptr)return false;
+        ListNode* rabbit = head;
+        ListNode* tort = head;
+        while(rabbit != nullptr  ){
+            rabbit = rabbit->next;
+
+            if(rabbit != nullptr){
+                
+                rabbit = rabbit->next;
+                tort = tort->next;
+
+                if(rabbit != nullptr && rabbit == tort){
+                    return true;
+                }
+            
             }
-
-            map[cur] = true;
-            cur = cur->next;
-
         }
         return false;
     }
