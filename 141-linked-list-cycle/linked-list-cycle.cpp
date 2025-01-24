f66aa5654f951bbdf7f3e9b7354557425116e4e9
Shivam <74777531+Shivam-officials@users.noreply.github.com>
2025-01-24 10:57:09 +0530

    Time: 6 ms (90.71%) | Memory: 11.9 MB (51.95%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,46 +1,38 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode(int x) : val(x), next(NULL) {}
+ *};
  */
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        // unordered_map<ListNode*,bool> map;
-        // ListNode* cur = head;
-        // while(cur != nullptr){
-        //     if(map.find(cur) != map.end() ){
-        //         return true;
-        //     }
+class Solution
+{
+    public:
+        bool hasCycle(ListNode *head)
+        {
 
-        //     map[cur] = true;
-        //     cur = cur->next;
-
-        // }
-        // return false;
-
-        //edge cases
-        if(head == nullptr)return false;
-        if(head->next==nullptr)return false;
-        ListNode* rabbit = head;
-        ListNode* tort = head;
-        while(rabbit != nullptr  ){
-            rabbit = rabbit->next;
-
-            if(rabbit != nullptr){
-                
+           	//edge cases
+            if (head == nullptr) return false;
+            if (head->next == nullptr) return false;
+            ListNode *rabbit = head;
+            ListNode *tort = head;
+            while (rabbit != nullptr)
+            {
                 rabbit = rabbit->next;
-                tort = tort->next;
 
-                if(rabbit != nullptr && rabbit == tort){
-                    return true;
+                if (rabbit != nullptr)
+                {
+
+                    rabbit = rabbit->next;
+                    tort = tort->next;
+
+                    if (rabbit == tort)
+                    {
+                        return true;
+                    }
                 }
-            
             }
+            return false;
         }
-        return false;
-    }
 };
